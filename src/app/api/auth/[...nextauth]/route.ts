01053b49cc41bf574171ba4808df0e41c50f4eb9
Marcelllombm <melombm@hotmail.com>
2023-07-16 13:10:31 -0300

    chore: add next auth prisma config

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import {Adapter} from "next-auth/adapters";
+
+const { prisma }: any = "@/lib/prisma";
+
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+    throw new Error("GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET must be defined");
+  }
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+      ],
+}
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST};
